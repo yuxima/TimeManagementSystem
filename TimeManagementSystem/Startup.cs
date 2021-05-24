30f9335c9faa74e66f907ccc71eef5b8fa08773e
Nastia Yukhymchuk <yuximchuk2111@gmail.com>
2021-05-25 00:06:27 +0300

    Added Identity

--- TimeManagementSystem/Startup.cs
+++ TimeManagementSystem/Startup.cs
@@ -1,12 +1,12 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using TimeManagementSystem.Data.Entities;
+using TimeManagementSystem.Data.Implementation;
 
 namespace TimeManagementSystem
 {
@@ -22,6 +22,12 @@ namespace TimeManagementSystem
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<ApplicationContext>(options =>
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<User, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationContext>();
+
             services.AddControllersWithViews();
         }
 
